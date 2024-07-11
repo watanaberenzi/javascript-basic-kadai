f3649aad97ec05865c5b65e415538ea8b4076c56
izumi misogi <147458040+watanaberenzi@users.noreply.github.com>
2024-07-11 15:15:42 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,17 @@
+const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+
+for (let i = 0; i < holidays.length; i++) {
+    console.log(holidays[i]);
+}
+
+let n = -1;
+let holidaysName = holidays[n];
+
+while (holidaysName !== "勤労感謝の日") {
+    n = n + 1;
+    if (n >= holidays.length) {
+        break;
+    }
+    holidaysName = holidays[n];
+    console.log(holidaysName);
+}
