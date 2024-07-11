a503ce41c210ad6998fca2561bd761e993e1e5fd
izumi misogi <147458040+watanaberenzi@users.noreply.github.com>
2024-07-11 14:44:55 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+let num;
+
+num = 100
+
+if (num % 15 == 0){
+    console.log("定数numは3と5の倍数です")
+} 
+else if (num % 5 == 0){
+    console.log("定数numは5の倍数です")
+}
+else if (num % 3 == 0){
+    console.log("定数numは3の倍数です")
+}
+else  {
+    console.log(num)
+}
